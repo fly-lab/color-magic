1eb66f10ee2d7e639811c15bbb415c2037f57e4f
DHN Chandan <php.chandan@gmail.com>
2023-03-17 23:37:06 +0600

    feat: :sparkles: Add utils

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+export const clamp = (value: number, min: number, max: number): number => Math.min(Math.max(value, min), max);
+
+export const safeRgb = (value: number): number => clamp(value, 0, 255);
+
+export const safeHex = (value: number): number => clamp(value, 0x00, 0xff);
+
+export const safeHue = (value: number): number => clamp(value, 0, 360);
+
+export const safePct = (value: number): number => clamp(value, 0, 100);
+
+export const safeAlpha = (value: number): number => clamp(value, 0, 1);
